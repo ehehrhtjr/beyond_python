acdb5697b572da054bb22aec75d2165d143ff74a
ehehrhtjr <107744549+ehehrhtjr@users.noreply.github.com>
2022-10-23 17:34:07 +0900

    Create 10_23.py

---
+++ 10_23.py
@@ -0,0 +1,48 @@
+import pygame
+
+pygame.init()
+
+screen=pygame.display.set_mode((500,500))
+run=True
+
+position=[0,0,100,100]
+position2=[400,400,100,100]
+
+
+while run:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            run=False
+
+        if event.type==pygame.KEYDOWN:
+            if event.key==pygame.K_RIGHT:
+                if position[0]<400:
+                    position[0]+=50
+                else:
+                    position[0]=0
+            
+            if event.key==pygame.K_UP:
+                if position[1]>0:
+                    position[1]-=50
+                else:
+                    position[1]=400
+                
+            if event.key==pygame.K_LEFT:
+                if position[0]>0:
+                    position[0]-=50
+                else:
+                    position[0]=400
+                    
+            if event.key==pygame.K_DOWN:
+                if position[1]<400:
+                    position[1]+=50
+                else:
+                    position[1]=0
+    
+    screen.fill((0,0,0))
+    pygame.draw.rect(screen,(255,0,0),position)
+    pygame.draw.rect(screen,(0,0,255),position2)
+    
+    pygame.display.update()
+
+pygame.quit()
